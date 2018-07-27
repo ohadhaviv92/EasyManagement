18becf0158d71b760d9e1515dfea5a0d99c43253
Orhay Benaim <orhaybenaim@gmail.com>
2018-07-27 15:57:12 -0700

    added Menu component

---
+++ easy_management_react/src/component/Menu/Menu.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import MenuIcon from './MenuIcon';
+import './Menu.css'
+export default class Menu extends Component {
+
+    render() {
+        return (
+            <MenuIcon />
+        )
+    }
+}
