c02112196139edfcef857f52e8536a1c8f4791c7
Orhay Benaim <orhaybenaim@gmail.com>
2018-08-01 21:14:32 +0300

    .

--- easy_management_react/src/component/Login/Login.jsx
+++ easy_management_react/src/component/Login/Login.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import SmartButton from '../SmartButton/SmartButton';
-import SQL from '../../Handlers/SQL';
 import './Login.css';
 
 export default class Login extends Component {
