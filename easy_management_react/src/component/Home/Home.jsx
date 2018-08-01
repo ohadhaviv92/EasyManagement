ba1a8b2b94c8ac25a41c587f9c4bb1a473251343
Ohad Haviv <ohadhaviv92@gmail.com>
2018-08-01 17:57:58 +0300

    ,\

--- easy_management_react/src/component/Home/Home.jsx
+++ easy_management_react/src/component/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
+
   render() {
     return (
       <div>
+        
         Home
+        <br/>
+        <button>Open Modal</button>
+       
+
      </div>
     )
   }
 }
+
