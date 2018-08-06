920e9b42dc7d0ede31bfb983f14cb9a8afc519da
Ohad Haviv <ohadhaviv92@gmail.com>
2018-08-06 18:31:19 +0300

    adde BAL and DAL

--- easy_management_react/src/component/SmartButton/SmartButton.jsx
+++ easy_management_react/src/component/SmartButton/SmartButton.jsx
@@ -3,32 +3,29 @@ import React, { Component } from 'react'
 import './SmartButton.css';
 
 
-
-
-
 export default class SmartButton extends Component {
 
-    state = {activeClass: 'login__submit', animating: false}
-    onLogin = async (e) => {
-        if (this.state.animating) return;
+  state = { activeClass: 'login__submit', animating: false }
+  onLogin = async (e) => {
+    if (this.state.animating) return;
 
-        this.setState({activeClass: "login__submit processing", animating: true});
+    this.setState({ activeClass: "login__submit processing", animating: true });
 
-        try {
-          const result = await this.props.Async();
-          this.setState({activeClass: "login__submit processing success"});
-          setTimeout( ()=> {
-            this.setState({activeClass: "login__submit", animating: false});
-            this.props.Action(result);
-          }, 500);
-        } catch (error) {
-          console.log(error);
-          this.setState({activeClass: "login__submit", animating: false});
-          
-        }
-      
-      
+    try {
+      const result = await this.props.Async();
+      this.setState({ activeClass: "login__submit processing success" });
+      setTimeout(() => {
+        this.setState({ activeClass: "login__submit", animating: false });
+        this.props.Action(result);
+      }, 500);
+    } catch (error) {
+      console.log(error);
+      this.setState({ activeClass: "login__submit", animating: false });
+
     }
+
+
+  }
 
 
   render() {
